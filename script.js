c862df759ca2cc47d5960158b0f44ef907287631
Akeem Flavors <akeemflavors@gmail.com>
2020-05-26 16:37:42 -0400

    On click for location-button to display API response

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Initial array of locations
 var locations = ["Charlotte,NorthCarolina", "Bujumbura,Burundi"];
 var APIKey = "5ac6ffcaa85c9e2fcacb6fac2c601688";
+var currentDayAndTime = moment().format('LLLL');
 // URL we need to query the database
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + APIKey;
 
@@ -34,6 +35,32 @@ $("#search-location-button").on("click", function(event){
     var locationInput = $("#search-location-input").val().trim();
     locations.push(locationInput);
     console.log(locations);
-
+// Render Updated List of Locations
     renderLocationButtons();
+})
+
+//Display Location Information
+$(document).on("click", ".location-button", function(){
+    $("#location-weather-view").empty();
+    var locationInputText = $(this).text();
+    var locationQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + locationInputText + "&appid=" + APIKey;
+    $.ajax({
+        url: locationQueryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        var locationName = $("<h1>");
+        var currentDate = $("<p>");
+        var temperature = $("<p>");
+        var uvIndex = $("<p>");
+        //Add 5 day forecast
+        $(locationName).text(response.name);
+        $("#location-weather-view").append(locationName);
+        $(currentDate).text(currentDayAndTime);
+        $("#location-weather-view").append(currentDate);
+        $(temperature).text(response.main.temp);
+        $("#location-weather-view").append(temperature);
+        $(uvIndex).text("TBD") //Figure out UV Index Value
+        $("#location-weather-view").append(uvIndex);
+    })
 })
