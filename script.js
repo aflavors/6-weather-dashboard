b2f5843819c36e86a547103b31665d6d2e906339
Akeem Flavors <akeemflavors@gmail.com>
2020-05-26 16:11:54 -0400

    On click for search-location-button

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Initial array of locations
-var locations = ["Charlotte,NorthCarolina", "Bujumbura,Burundi"]
+var locations = ["Charlotte,NorthCarolina", "Bujumbura,Burundi"];
 var APIKey = "5ac6ffcaa85c9e2fcacb6fac2c601688";
 // URL we need to query the database
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&appid=" + APIKey;
@@ -11,15 +11,29 @@ $.ajax({
   method: "GET"
 }).then(function(response) {
     console.log(response);
+    console.log(response.name)
 });
-// //Test display locations
-// function renderLocationButtons() {
-//     $("#search-location-button-list").empty();
-//     locations.forEach(function(location){
-//         var newLocationButton = $("<button>");
-//         newLocationButton.text(location);
-//         newLocationButton.addClass("location-button");
-//         $("#search-location-button-list").append(newLocationButton);
-//     })
-// }
-// renderLocationButtons();
+//Test display locations
+function renderLocationButtons() {
+    $("#search-location-button-list").empty();
+    locations.forEach(function(location){
+        var newLocationButton = $("<button>");
+        newLocationButton.text(location);
+        newLocationButton.addClass("location-button");
+        newLocationButton.addClass("btn");
+        newLocationButton.addClass("btn-outline-secondary");
+        newLocationButton.addClass("btn-group-vertical");
+        $("#search-location-button-list").append(newLocationButton);
+    })
+}
+renderLocationButtons();
+
+//Add Location to Array and Render on List
+$("#search-location-button").on("click", function(event){
+    event.preventDefault();
+    var locationInput = $("#search-location-input").val().trim();
+    locations.push(locationInput);
+    console.log(locations);
+
+    renderLocationButtons();
+})
